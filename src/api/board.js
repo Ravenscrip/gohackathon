7b6ec532e7a572a38c11ddbfc412d6e8fa92cd0a
Vlad Spiridonov <xxjadxx14@gmail.com>
2021-05-01 15:36:12 +0300

    Add centaur_token var

--- src/api/board.js
+++ src/api/board.js
@@ -1,5 +1,5 @@
 import { GET } from "./base";
-const CENTAUR_TOKEN = 'test1'
+const CENTAUR_TOKEN = 'sDRRrRGhfGW7dW3e'
 
 export const helpBestMoves = (token, game_id, count) => {
   return GET(`hints/best-moves?token=${token}&game_id=${game_id}&centaur_token=${CENTAUR_TOKEN}&count=${count}`, {}, token);
@@ -10,21 +10,21 @@ export const helpBestMovesEnemy = (token, game_id, count) => {
 };
 
 export const helpShowBest = (token, game_id, moves) => {
-  return GET(`hints/show-best?game_id=${game_id}&moves=${moves}&centaur_token=test1&token=${token}`, {}, token);
+  return GET(`hints/show-best?game_id=${game_id}&moves=${moves}&centaur_token=${CENTAUR_TOKEN}&token=${token}`, {}, token);
 };
 
 export const helpHeatmapFull = (token, game_id) => {
-  return GET(`hints/heatmap-full?game_id=${game_id}&centaur_token=test1&token=${token}`, {}, token);
+  return GET(`hints/heatmap-full?game_id=${game_id}&centaur_token=${CENTAUR_TOKEN}&token=${token}`, {}, token);
 };
 
 export const helpHeatmapZone = (token, game_id, is_quarter) => {
-  return GET(`hints/heatmap-best-move-zone?game_id=${game_id}&centaur_token=test1&token=${token}&is_quarter=${is_quarter ? 1 : 0}`, {}, token);
+  return GET(`hints/heatmap-best-move-zone?game_id=${game_id}&centaur_token=${CENTAUR_TOKEN}&token=${token}&is_quarter=${is_quarter ? 1 : 0}`, {}, token);
 };
 
 export const helpHeatmapQuarter = (token, game_id, quarter) => {
-  return GET(`hints/heatmap-quarter?game_id=${game_id}&centaur_token=test1&token=${token}&quarter=${quarter}`, {}, token);
+  return GET(`hints/heatmap-quarter?game_id=${game_id}&centaur_token=${CENTAUR_TOKEN}&token=${token}&quarter=${quarter}`, {}, token);
 };
 
 export const scoresWinner = (token, game_id) => {
-  return GET(`hints/winner?game_id=${game_id}&centaur_token=test1&token=${token}`, {}, token);
+  return GET(`hints/winner?game_id=${game_id}&centaur_token=${CENTAUR_TOKEN}&token=${token}`, {}, token);
 };
